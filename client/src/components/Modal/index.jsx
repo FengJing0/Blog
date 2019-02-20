15db5a6c006bf96a97e2389d58a9b24da6f0f192
FengJing <448195466@qq.com>
2019-02-20 22:33:19 +0800

    Modal

--- client/src/components/Modal/index.jsx
+++ client/src/components/Modal/index.jsx
@@ -1,21 +1,58 @@
-import React,{PureComponent} from 'react'
+import React,{PureComponent,Fragment} from 'react'
 import PropTypes from 'prop-types';
-import {Modal as antModal} from 'antd'
+import {Button, Input, Modal as AntModal} from 'antd'
 
 class Modal extends PureComponent{
   state = {
-    show:false
+    visible:false
   }
 
   static propTypes = {
     title:PropTypes.string.isRequired,
-    form:PropTypes.object.isRequired,
+    formList:PropTypes.array.isRequired,
     submit:PropTypes.func.isRequired,
     cancel:PropTypes.func.isRequired
   }
 
+  handleShowModel = () => {
+    this.setState({visible: true})
+  }
+
+  handleClose = () => {
+
+  }
+
+  handleSubmit = () => {
+    this.setState({visible: false})
+    this.props.submit()
+  }
+
+  getModel = () => {
+    const {visible}  = this.state
+    const {title,formList}  = this.props
+    return (<AntModal
+        title={title}
+        visible={visible}
+        onOk={this.handleSubmit}
+        onCancel={() => {this.setState({visible: false})}}
+        afterClose={this.handleClose}
+        okText="确认"
+        cancelText="取消">
+      {
+        formList.map(item => <Input key={item.label}
+                                    placeholder={item.placeholder}
+                                    value={item.value}
+                                    onChange={e => {item.change(e.value)}}/>)
+      }
+
+    </AntModal>)
+  }
+
   render() {
-    return <div/>
+    return <Fragment>
+      {this.getModel()}
+      <Button htmlType='button' onClick={this.handleShowModel}>{this.props.children}</Button>
+    </Fragment>
   }
 }
 
